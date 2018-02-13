cb642e2186172adede355f481ce60003d18eb9b6
Mariya Ishchuk <mariya.ishchuk@me.com>
2018-02-14 01:04:29 +0200

    homework_07_02

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,39 +1,40 @@
-var wantPlay = true
-var prize = 0;
-var attempt = 1;
-var firstTimePrize = 10;
-var secondTimePrize = 5;
-var thirdTimePrize = 2;
+var wantPlay = true;
+var bank = 0;
+var maxRange = 5;
+var prize = 10;
+var localPrize;
+var random;
+var i;
 
 while (wantPlay) {
     wantPlay = confirm("Do you want to play a game?");
     if (wantPlay === false) {
         console.log("You did not become a millionaire");
     } else {
-        var random = Math.floor(Math.random() * (5 * attempt) + 1);
-        for (var i = 0; i < 3; i ++) {
-            if (random === Number(prompt("Enter number in range 0-5"))) {
-                if (i === 0) {
-                    prize += firstTimePrize;
+        random = Math.floor(Math.random() * (maxRange + 1));
+        localPrize = prize;
+        for (i = 3; i > 0; i = i - 1) {
+            if (random === Number(prompt("Enter number in range 0-" + maxRange + "\n Attempts left: " + i + "\n Total prize: " + bank + "$\n Total prize on current attempt: " + localPrize + "$"))) {
+                if (i === 3) {
                     console.log("You guessed number on 1-st attempt! Your prize is 10$ - maximum prize for current numbers range");
-                } else if (i === 1) {
-                    prize += secondTimePrize;
-                    console.log ("You guessed number on 2-nd attempt! Your prize is 5$");
+                } else if (i === 2) {
+                    console.log("You guessed number on 2-nd attempt! Your prize is 5$");
                 } else {
-                    prize += thirdTimePrize;
-                    console.log ("You guessed number on 3-rd attempt! Your prize is 2$");
+                    console.log("You guessed number on 3-rd attempt! Your prize is 2$");
                 }
+                bank += localPrize;
+                maxRange *= 2;
+                prize *= 3;
                 break;
             } else {
-                firstTimePrize = Math.floor(firstTimePrize / 2);
-                secondTimePrize = Math.floor(secondTimePrize / 2);
-                thirdTimePrize = Math.floor(thirdTimePrize / 2);
-                console.log ("Thank you for a game. Your prize is: " + prize + "$");
+                console.log("Thank you for a game. Your prize is: " + bank + "$");
+                if (i === 1) {
+                    bank = 0;
+                    maxRange = 5;
+                    prize = 10;
+                }
             }
+            localPrize = Math.floor(localPrize / 2);
         }
     }
-    attempt++;
-    firstTimePrize = firstTimePrize * 3;
-    secondTimePrize = secondTimePrize * 3;
-    thirdTimePrize = thirdTimePrize * 3;
 }
